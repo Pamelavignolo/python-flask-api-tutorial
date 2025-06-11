5d0ccecdaab89179d17c4b98cac266a8e1bd12c4
Pamelavignolo <136765465+Pamelavignolo@users.noreply.github.com>
2025-06-11 20:21:03 +0000

    trabajo terminado

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask import request
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False }
+]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json()
+    print("Incoming request with the following body", request_body)
+    
+    todos.append(request_body)
+    
+    return jsonify(todos) 
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Invalid position"}), 400
+    todos.pop(position)
+    return jsonify(todos), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
